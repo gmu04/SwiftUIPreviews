29843ce4ad5035c578b51748989f50f9a95af844
GokhanMutlu <gokhanmutlu04@yahoo.com>
2022-01-12 12:38:41 +0300

    Initial Commit

---
+++ XcodePreviews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XcodePreviews
+//
+//  Created by Gokhan Mutlu on 12.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XcodePreviews/XcodePreviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodePreviewsApp.swift
+//  XcodePreviews
+//
+//  Created by Gokhan Mutlu on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodePreviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
